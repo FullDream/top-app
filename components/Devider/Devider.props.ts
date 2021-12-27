b2e96c4bdcccac5709d39c485c26307243ad4f63
Pavel Rybakin <pvrybakin@gmail.com>
2021-12-27 23:42:38 +0300

    divider fix

--- components/Devider/Devider.props.ts
+++ components/Devider/Devider.props.ts
@@ -1,5 +1,5 @@
 import { DetailedHTMLProps, HTMLAttributes } from "react";
 
-export interface Deviderprops extends DetailedHTMLProps<HTMLAttributes<HTMLHRElement>, HTMLHRElement> {
+export interface DeviderProps extends DetailedHTMLProps<HTMLAttributes<HTMLHRElement>, HTMLHRElement> {
     
 }
